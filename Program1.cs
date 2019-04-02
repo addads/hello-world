7bb56380f35c5c5db96b539497a980f6d9c5560e
addads <43000091+addads@users.noreply.github.com>
2019-04-02 14:37:23 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security;
+using System.Security.Cryptography;
+
+
+namespace ConsoleApp2
+{
+    class Student
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public int ID { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public List<int> Scores;
+    }
+
+    class Teacher
+    {
+        public string First { get; set; }
+        public string Last { get; set; }
+        public int ID { get; set; }
+        public string City { get; set; }
+    }
+
+    class Program
+    {
+        public static string Encrypt(string data)
+        {
+            TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
+
+            DES.Mode = CipherMode.ECB;
+            DES.Key = Convert.FromBase64String("AAECAwQFBgcICQoLDA0ODw==");
+            DES.Padding = PaddingMode.PKCS7;
+            ICryptoTransform DESEncrypt = DES.CreateEncryptor();
+            Byte[] Buffer = ASCIIEncoding.ASCII.GetBytes(data);
+
+            return Convert.ToBase64String(DESEncrypt.TransformFinalBlock(Buffer, 0, Buffer.Length));
+        }
+
+        public static string Decrypt(string data)
+        {
+            TripleDESCryptoServiceProvider DES = new TripleDESCryptoServiceProvider();
+
+            DES.Mode = CipherMode.ECB;
+            DES.Key = Convert.FromBase64String("AAECAwQFBgcICQoLDA0ODw==");
+
+            DES.Padding = PaddingMode.PKCS7;
+            ICryptoTransform DESEncrypt = DES.CreateDecryptor();
+            Byte[] Buffer = Convert.FromBase64String(data.Replace(" ", "+"));
+
+            return Encoding.UTF8.GetString(DESEncrypt.TransformFinalBlock(Buffer, 0, Buffer.Length));
+        }
+
+        static void Main(string[] args)
+        {
+            string pass = Encrypt("addads");
+            Console.WriteLine(pass);
+            Console.WriteLine(Decrypt(pass));
+
+            int[] numbers = new int[7] { 0, 1, 2, 3, 4, 5, 6 };
+
+            var numQuery = (from num in numbers where (num % 2) == 0 select num).ToArray();
+
+            foreach (int n in numQuery)
+            {
+                Console.WriteLine(n);
+            }
+
+            List<Student> students = new List<Student>()
+        {
+            new Student { First="Svetlana",
+                Last="Omelchenko",
+                ID=111,
+                Street="123 Main Street",
+                City="Seattle",
+                Scores= new List<int> { 97, 92, 81, 60 } },
+            new Student { First="Claire",
+                Last="O’Donnell",
+                ID=112,
+                Street="124 Main Street",
+                City="Redmond",
+                Scores= new List<int> { 75, 84, 91, 39 } },
+            new Student { First="Sven",
+                Last="Mortensen",
+                ID=113,
+                Street="125 Main Street",
+                City="Lake City",
+                Scores= new List<int> { 88, 94, 65, 91 } },
+        };
+
+            List<Teacher> teachers = new List<Teacher>()
+        {
+            new Teacher { First="Ann", Last="Beebe", ID=945, City="Seattle" },
+            new Teacher { First="Alex", Last="Robinson", ID=956, City="Redmond" },
+            new Teacher { First="Michiyo", Last="Sato", ID=972, City="Tacoma" }
+        };
+
+            var peopleInSeattle = (from student in students
+                                   where student.City == "Seattle"
+                                   select student.Last)
+                        .Concat(from teacher in teachers
+                                where teacher.City == "Seattle"
+                                select teacher.Last);
+
+            Console.WriteLine("The following students and teachers live in Seattle:");
+
+            foreach (var person in peopleInSeattle)
+            {
+                Console.WriteLine(person);
+            }
+
+
+
+            double[] radii = { 1, 2, 3 };
+
+            IEnumerable<string> query =
+                from rad in radii
+                select $"Area = {rad * rad * Math.PI:F2}";
+
+            foreach (string s in query)
+                Console.WriteLine(s);
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
